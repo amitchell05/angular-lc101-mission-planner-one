ee6eaac193badef2c1273082be75792b51718dc4
Angela Mitchell <anglarenmit@gmail.com>
2022-03-06 22:26:25 -0600

    complete studio

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,7 @@ export class HeaderComponent implements OnInit {
   missionName: string = 'Mars 2030';
   rocketName: string = 'Plasma Max';
   editingMission: boolean = false;
+  editingRocket: boolean = false;
 
   constructor() {}
 
@@ -17,5 +18,10 @@ export class HeaderComponent implements OnInit {
   updateMission(updatedName: string) {
     this.missionName = updatedName;
     this.editingMission = false;
+  }
+
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
   }
 }
